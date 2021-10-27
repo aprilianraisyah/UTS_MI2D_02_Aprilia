21e174cfcadb27954ce4c921cc20069541edfa5d
apriliaisyah <aprilianraisyah@gmailcom>
2021-10-27 19:29:19 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Wisata Edukasi - Kampung Coklat</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Sistem Informasi Wisata Edukasi Kampung Coklat</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#destination">Destinasi</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#navigation">Navigasi</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">Kampung Coklat</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Pusat Sistem Informasi Wisata Edukasi Kampung Cokelat di Kabupaten Blitar</h2>
+                        <a class="btn btn-primary" href="#about">Click to Start</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <!-- About-->
+        <section class="about-section text-center" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">Wisata Edukasi - Kampung Coklat</h2>
+                        <p class="text-white-50">
+                     Kampung Coklat merupakan salah satu wisata yang berada di Kabupaten Blitar. Kampung Wisata Edukasi yang 
+                    berdiri sejak 17 Agustus 2014 ini memiliki harapan mampu memberikan pengetahuan tentang budi daya tanaman 
+                    kakao hingga pengolahan cokelat. Setiap harinya pengunjung Kampung Coklat mencapai 1000 orang. 
+                    Baik untuk berwisata edukasi ataupun menikmati olahan cokelat. <br><br>
+                    Beberapa tempat wisata yang ada pada Kampung Coklat seperti terapi ikan, pembenihan coklat
+                    galery coklat serta kafetaria dengan berbagai olahan makanan serta minuman berbahan dasar cokelat. <br><br>
+                    Kampung Wisata edukasi Kampung Cokelat buka pada : <br>    
+                    a. Weekday dengan tiket : Rp. 10.000 per orang (dewasa)<br>
+                    b. Weekend dengan tiket : Rp. 15.000 per orang (dewasa)<br>
+                    Dengan jam operasional mulai pukul 08.00 - 17.00 WIB <br> </p>
+                    
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Destination-->
+        <section class="destination-section bg-light" id="destination">
+            <div class="container px-4 px-lg-5">
+                <!-- Terapi Ikan -->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../img/terapiikan.jpg" width="696" height="400" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Terapi Ikan</h4>
+                            <p class="text-black-50 mb-0">
+                                Terapi ikan merupakan salah satu destinasi yang selalu ramai dikunjungi.<br>
+                                Terapi ikan memiliki berbagai manfaat seperti menghaluskan kulit sampai memperlancar sirkulasi darah.<br>
+                                Tak heran jika pengunjung destinasi ini cukup banyak dari anak - anak sampai usia lanjut.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- Pembenihan Coklat -->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../img/pembenihan2.jfif" width="696" height="400" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Pembenihan Coklat</h4>
+                            <p class="text-black-50 mb-0">
+                            Destinasi pembenihan coklat merupakan tempat dimana benih - benih pohon kakao dikembangkan.<br>
+                            Pengunjung dapat melihat segala macam benih yang digolongkan berdasarkan umurnya.<br>
+                            Terdapat peraturan bahwa pengunjung dilarang merusak tanaman kakao, sehingga hanya diperbolehkan untuk mengamati atau berfoto didekatnya.<br> 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Galery Coklat -->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../img/galerycoklat.jpg" width="696" height="400" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Galery Coklat</h4>
+                            <p class="text-black-50 mb-0">
+                            Destinasi galery coklat adalah pusat oleh - oleh berbagai jenis olahan coklat.<br>
+                            Terdapat beberapa olahan seperti permen coklat, coklat dengan berbagai jenis rasa, dan lainnya.<br>
+                            Tempat ini dapat dikunjungi oleh pengunjung yang ingin memberikan buah tangan khas dari tempat wisata Kampung Coklat.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Kafetaria -->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../img/kafetaria.jpg" width="696" height="400" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Kafetaria</h4>
+                            <p class="text-black-50 mb-0">
+                            Kafetaria merupakan destinasi yang tidak pernah sepi diantara destinasi lainnya di Kampung Coklat<br>
+                            Pengunjung dapat menikmati berbagai macam olahan makanan juga minuman berbahan dasar coklat.<br>
+                            Terdapat mie coklat, minuman ice chocolate, dan sate buah coklat.<br>
+                            Sambil menikmati hidangannya, pengunjung bisa bersantai atau berswafoto di bawah pohon kakao yang rindang.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- Navigation -->
+        <section class="navigation-section" id="navigation">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-10 col-lg-8 mx-auto text-center">
+                        <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+                        <h2 class="text-white mb-4">Navigasi Lokasi Kampung Coklat</h2>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.4556421489137!2d112.16960131406552!3d-8.156763383971308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78ea495a3afc23%3A0xcb37ac5a146a3533!2sWisata%20Edukasi%20Kampung%20Coklat!5e0!3m2!1sid!2sid!4v1635330497824!5m2!1sid!2sid" 
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Contact-->
+        <section class="contact-section bg-black" id="contact">
+            <div class="justify-content-center">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Alamat</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">Jl. Bantengblorok No.18 Kec. Kademangan Kab. Blitar Jawa Timur</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0" >
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Telepon</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">Reservasi : 0822-2056-7818</div>
+                                <div class="small text-black-50">Pemesanan : 0822-2777-4838</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50"><a href="#!">kampungcoklatblitar@gmail.com</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; MI2D Aprilia Noor A'isyah</div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
